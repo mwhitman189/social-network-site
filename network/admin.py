efe07ca9622007c46a78ecfac0e917371b689200
mWhitman189 <mwhitman189@gmail.com>
2020-10-13 18:57:00 +0900

    fix: Remove cached python files from git

--- network/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User, Post, Like, Comment, Follower
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Post)
-admin.site.register(Like)
-admin.site.register(Comment)
-admin.site.register(Follower)
