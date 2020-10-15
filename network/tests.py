11ea694522f577d727f127be7e18dbcf0d78fb9f
mWhitman189 <mwhitman189@gmail.com>
2020-10-15 18:53:51 +0900

    fix: Fix broken DB connection

--- network/tests.py
+++ network/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
 
+from .models import User, Post, Comment, Follower, Like
+
 # Create your tests here.
+
+
+class NetworkTestCase(TestCase):
+    """ Test cases for the social network app """
+
+    def setUp(self):
+        """ Initialize objects in the test DB """
+
+        # Create users
+        u1 = User.objects.create(
+            username="Ronald", password="MacInDaHouse", is_superuser=True)
+        u2 = User.objects.create(username="Dennis", password="GettinDenised")
+        u3 = User.objects.create(username="SweetDee", password="You'reAbird")
+
+        # Create posts
+        p1 = Post.objects.create(
+            user=u1, content="What will we do tonight, Brain?")
+        p2 = Post.objects.create(
+            user=u2, content="The same thing we do every night, Pinky...")
+        p3 = Post.objects.create(user=u3, content="This is a suuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuupppppppeeeeeeeeeeeeeeeeeeeeeeerrrrrrrrrrrrrrrrrr Loooooooooooooonnnnnnnnnnnnnnnnnnnnngggggggggggggggg ppppppppppppppppppppooooooooooooooooooooooossssssssssssssssssssssssstttttttttttttttt")
+        p4 = Post.objects.create(
+            user=u1, content="Try to take over the world?...")
+        p5 = Post.objects.create(
+            user=u2, content="No, Pinky. Try to take over the world!!!!")
+
+        # Create comments
--- project4/settings.py
+++ project4/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'social_network',
+        'NAME': 'network',
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': 'db',
